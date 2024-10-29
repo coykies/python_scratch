6a8396f0d2f7c49495871ec8628ce1d517b9a316
coykies <it.just.cookies@gmail.com>
2024-10-29 20:57:11 +0300

    add division in var w and format

--- calculator.py
+++ calculator.py
@@ -4,7 +4,11 @@ y = float(input("What's y? "))
 
 #adding number and round to bigger side
 z = round(x + y)
+#add division in var "w"
+w = x / y
 
 #formating z to us standart (for example 1,000)
 print(f"{z:,}")
 
+#format w to two digits after dot
+print(f"{w:.2f}")
