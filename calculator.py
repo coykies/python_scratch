7dc77cd2699f4e3fa4c438fd799a3774b1dc66f9
coykies <it.just.cookies@gmail.com>
2024-10-29 21:31:04 +0300

    calc with func square

--- calculator.py
+++ calculator.py
@@ -1,14 +1,9 @@
-#ask user number x and y 
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-
-#adding number and round to bigger side
-z = round(x + y)
-#add division in var "w"
-w = x / y
-
-#formating z to us standart (for example 1,000)
-print(f"{z:,}")
-
-#format w to two digits after dot
-print(f"{w:.2f}")
+#func with main code
+def main ():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+#func square 
+def square(n):
+    return n * n
+#output func main
+main()
