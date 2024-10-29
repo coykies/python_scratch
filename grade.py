cbb29d5b5f28949962d4514b168186a9110e1df8
coykies <it.just.cookies@gmail.com>
2024-10-29 22:24:13 +0300

    optimazed grade v1

--- grade.py
+++ grade.py
@@ -2,16 +2,16 @@
 score = int(input("Score: "))
 
 #grade A
-if 90 <= score <= 100:
+if score >= 100:
     print("Grade: A")
 #grade B
-elif 80 <= score < 90:
+elif score >= 90:
     print("Grade: B")
 #grade C
-elif 70 <= score < 80:
+elif score >= 80:
     print("Grade: C")
 #grade D
-elif 60 <= score < 70: 
+elif score >= 70: 
     print("Grade: D")
 #grade F
 else: 
