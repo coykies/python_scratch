faa60c112fbafdf298ad32c1421be5e82874e045
coykies <it.just.cookies@gmail.com>
2024-10-29 21:45:24 +0300

    add compare.py

---
+++ compare.py
@@ -0,0 +1,10 @@
+#ask user what is var x and y
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less that y")
+if x > y: 
+    print("x is greater than y")
+if x == y: 
+    print("x is equal to y")
