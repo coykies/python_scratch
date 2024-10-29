bc8b01b9bbd82b98abeb8948e5ca504c925dc550
coykies <it.just.cookies@gmail.com>
2024-10-29 20:48:34 +0300

    add comment and formating

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
+#ask user number x and y 
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-print(x + y)
+#adding number and round to bigger side
+z = round(x + y)
+
+#formating z to us standart (for example 1,000)
+print(f"{z:,}")
 
