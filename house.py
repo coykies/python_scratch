807982ec45cd52882bdf2ef0c00de35ba99331ea
coykies <it.just.cookies@gmail.com>
2024-10-30 21:31:18 +0300

    add house.py from harry potter

---
+++ house.py
@@ -0,0 +1,16 @@
+#Where is live persons from Harry Potter
+
+#what is name person 
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione": 
+    print("Gryffindor")
+elif name == "Rone":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+    
