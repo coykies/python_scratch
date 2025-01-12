25f0eb9c814844aed032a68fa8e6756a2523058a
coykies <it.just.cookies@gmail.com>
2025-01-12 19:21:40 +0300

    modify def cat.py

--- cat.py
+++ cat.py
@@ -1,5 +1,13 @@
 def main():
-    meow(3)
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
 
 def meow(n):
     for _ in range(n):
