df00f78ccb053ebe812292e8d41f073b4ba10972
coykies <it.just.cookies@gmail.com>
2025-01-17 20:45:53 +0300

    add list in dict and patronus

--- hogwarts.py
+++ hogwarts.py
@@ -1,10 +1,10 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin",
-}
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
-for student in students: 
-    print(student, students[student], sep=", ")
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", "),
 
