f29bbf182cd6953c74cd32283968e5ba610614f0
coykies <it.just.cookies@gmail.com>
2025-01-29 22:26:39 +0300

    add var promt

--- number.py
+++ number.py
@@ -2,10 +2,10 @@ def main():
     x = get_int()
     print(f"x is {x}")
 
-def get_int():
+def get_int(promt):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input("promt"))
         except ValueError: 
             pass
 
