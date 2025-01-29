6d87160b305c55f47e0264b86c61f73baa121aae
coykies <it.just.cookies@gmail.com>
2025-01-29 22:30:28 +0300

    fix get_int

--- number.py
+++ number.py
@@ -1,11 +1,11 @@
 def main():
-    x = get_int()
+    x = get_int("What's is x ? ")
     print(f"x is {x}")
 
 def get_int(promt):
     while True:
         try:
-            return int(input("promt"))
+            return int(input(promt))
         except ValueError: 
             pass
 
