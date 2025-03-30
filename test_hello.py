0eea0859d2d0f405e3969285ed6da6d9c5753fb0
coykies <it.just.cookies@gmail.com>
2025-03-30 20:47:53 +0300

    add loop for test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -4,4 +4,5 @@ def test_default():
     assert hello() == "hello, world"
 
 def test_argument():
-     assert hello("David") == "hello, David"
+    for name in ["Hermione", "Harry", "Ron"]:
+        assert hello(name) == f"hello, {name}"
