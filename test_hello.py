c7ebf36db3adf820230ecccbd907155bef44afda
coykies <it.just.cookies@gmail.com>
2025-03-30 20:41:52 +0300

    creat two def in test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,7 @@
 from hello import hello
 
-def test_hello():
-    assert hello("David") == "hello, David"
+def test_default():
     assert hello() == "hello, world"
+
+def test_argument():
+     assert hello("David") == "hello, David"
