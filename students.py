b5daebfd269647503506562a93e9999ee92af991
coykies <it.just.cookies@gmail.com>
2025-04-02 21:25:50 +0300

    sorted key by house

--- students.py
+++ students.py
@@ -6,8 +6,8 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-def get_name(student):
-    return student["name"]
+def get_house(student):
+    return student["house"]
 
-for student in sorted(students, key=get_name, reverse=True):
+for student in sorted(students, key=get_house):
     print(f"{student['name']} is in {student['house']}")
