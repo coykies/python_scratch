7a9820deda8a8138c5284c6f3ff9dbd819e1d30a
coykies <it.just.cookies@gmail.com>
2025-04-02 21:04:55 +0300

    sorted name from students.csv

--- students.py
+++ students.py
@@ -1,4 +1,9 @@
-with open("students.csv") as file: 
-    for line in file:
+students = []
+
+with open("students.csv") as file:
+    for line in file: 
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print(student)
